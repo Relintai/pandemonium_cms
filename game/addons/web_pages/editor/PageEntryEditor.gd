f523f60cead1680c73fe0addf37c6935cbe67e28
Relintai <relintai@protonmail.com>
2022-08-25 11:03:21 +0200

    Better ui for the page entry.

--- game/addons/web_pages/editor/PageEntryEditor.gd
+++ game/addons/web_pages/editor/PageEntryEditor.gd
@@ -1,27 +1,38 @@
 tool
-extends PanelContainer
+extends VBoxContainer
 
 var _entry : WebPageEntry = null
 
+var _entry_type_label : Label = null
+
+signal entry_add_requested_after(entry)
+signal entry_move_up_requested(entry)
+signal entry_move_down_requested(entry)
+signal entry_delete_requested(entry)
+
 func set_entry(entry : WebPageEntry) -> void:
 	_entry = entry
+	
+	_entry_type_label.text = entry.get_class()
 
 func _on_add_button_pressed():
-	pass
+	emit_signal("entry_add_requested_after", _entry)
 	
 func _on_up_button_pressed():
-	pass
+	emit_signal("entry_move_up_requested", _entry)
 	
 func _on_down_button_pressed():
-	pass
+	emit_signal("entry_move_down_requested", _entry)
 	
 func _on_delete_button_pressed():
-	pass
+	emit_signal("entry_delete_requested", _entry)
 	
 func _notification(what):
 	if what == NOTIFICATION_INSTANCED:
-		get_node("VBoxContainer/HBoxContainer/AddButton").connect("pressed", self, "_on_add_button_pressed")
-		get_node("VBoxContainer/HBoxContainer/UpButton").connect("pressed", self, "_on_up_button_pressed")
-		get_node("VBoxContainer/HBoxContainer/DownButton").connect("pressed", self, "_on_down_button_pressed")
-		get_node("VBoxContainer/HBoxContainer/Delete").connect("pressed", self, "_on_delete_button_pressed")
+		_entry_type_label = get_node("PC/VBC/TopBar/EntryTypeLabel")
+		
+		get_node("AddButton").connect("pressed", self, "_on_add_button_pressed")
+		get_node("PC/VBC/TopBar/UpButton").connect("pressed", self, "_on_up_button_pressed")
+		get_node("PC/VBC/TopBar/DownButton").connect("pressed", self, "_on_down_button_pressed")
+		get_node("PC/VBC/TopBar/Delete").connect("pressed", self, "_on_delete_button_pressed")
 		
